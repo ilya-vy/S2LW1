b6c9e5aa475e475b2909793fd0b54c7176da784e
ilya-vy <70759382+ilya-vy@users.noreply.github.com>
2021-02-16 22:52:38 -0800

    Commit #2

--- Jan_21_LW_1_T_1/Jan_21_LW_1_T_1/Program.cs
+++ Jan_21_LW_1_T_1/Jan_21_LW_1_T_1/Program.cs
@@ -2,12 +2,12 @@
 
 namespace Jan_21_LW_1_T_1
 {
-    public delegate double DelegateA(double x, double y, double z);
+    public delegate int DelegateA(int x, int y, int z);
     class Program
     {
         static void Main(string[] args)
         {
-            DelegateA delA = delegate (double x, double y, double z) { return(x + y + z) / 3; };
+            DelegateA delA = delegate (int x, int y, int z) { return(x + y + z) / 3; };
             Console.WriteLine(delA(5, 5, 27));
             Console.ReadKey();
         }
--- Jan_21_LW_1_T_3/Jan_21_LW_1_T_3/Program.cs
+++ Jan_21_LW_1_T_3/Jan_21_LW_1_T_3/Program.cs
@@ -11,34 +11,36 @@ namespace Jan_21_LW_1_T_3
             Random rnd = new Random();
             return rnd.Next(1, 299);
         }
-        static MyDelegate[] delegates = new MyDelegate[] 
-        {
-            new MyDelegate(p1),
-            new MyDelegate(p1),
-            new MyDelegate(p1),
-            new MyDelegate(p1),
-            new MyDelegate(p1),
-        };
-        public class Program
-        {
-            static void Main(string[] args)
-            {
-                MyDelegate delA;
-                delA = delegate
-                {
-                    int y = 0;
-                    int sum = 0;
-                    for (int i = 0; i < delegates.Length; i++)
-                    {
-                        y++;
-                        sum = sum + delegates[i]();
-                    }
-                    return sum / y;
-                };
-                Console.WriteLine("Average value: {0}", delA());
-                Console.ReadKey();
-            }
-        }
+        
+        
     }
+    public class Program
+    {
+        static MyDelegate[] delegates = new MyDelegate[]
+        {
+            new MyDelegate(MyClass.p1),
+            new MyDelegate(MyClass.p1),
+            new MyDelegate(MyClass.p1),
+            new MyDelegate(MyClass.p1),
+            new MyDelegate(MyClass.p1),
+        };
+        static void Main(string[] args)
+        {
+            MyDelegate delA;
+            delA = delegate
+            {
+                int y = 0;
+                int sum = 0;
+                for (int i = 0; i < delegates.Length; i++)
+                {
+                    y++;
+                    sum = sum + delegates[i]();
+                }
+                return sum / y;
+            };
+            Console.WriteLine("Average value: {0}", delA());
+            Console.ReadKey();
+        }
+    } 
 }
 
